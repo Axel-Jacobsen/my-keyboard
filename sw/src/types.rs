27e33a8f85387fa0cbd9b57d51d25d50e5ac83f0
Axel Jacobsen <axelnj44@gmail.com>
2025-07-30 21:46:24 -0700

    ya

--- sw/src/types.rs
+++ sw/src/types.rs
@@ -13,21 +13,3 @@ pub struct Key {
     pub row_id: usize,
     pub col_id: usize,
 }
-
-impl Key {
-    pub fn to_ascii(&self) -> heapless::String<64> {
-        use core::fmt::Write;
-        let mut s = heapless::String::new();
-        let _ = write!(
-            &mut s,
-            "{}{:02}-{:02}\n",
-            match self.side {
-                Side::Left => "L",
-                Side::Right => "R",
-            },
-            self.row_id,
-            self.col_id
-        );
-        s
-    }
-}
